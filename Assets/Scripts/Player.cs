e050b7f4d92083615487ef521aecbc2a98328ee5
andrew09090870 <andrew09090870@yandex.ru>
2023-05-14 01:32:00 +0700

    Подкрутил прокачку игроку
    Работает: Максимальное хп
    Голда в сундуках
    Скорость

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -22,29 +22,57 @@ public class Player : MonoBehaviour
     public Image heart2;
     public Image heart1;
 
-
+    int MaxHealth;
+    int ms;
+    int gold;
     // Основной код персонажа
     void Start()
     {
-        PlayerPrefs.SetInt("MaxHealth", 0);
         rb = GetComponent<Rigidbody2D>();
+        // Прокачка
+        // хп
+        if (PlayerPrefs.GetInt("BonusHealth") == 2)
+        {
+            MaxHealth = 50;
+            health = 50;
+        }
+        else if (PlayerPrefs.GetInt("BonusHealth") == 1)
+        {
+            MaxHealth = 40;
+            health = 40;
+            heart5.enabled = false;
+        }
+        else
+        {
+            MaxHealth = 30;
+            health = 30;
+            heart4.enabled = false;
+            heart5.enabled = false; 
+        }
+        // скорость
+        if (PlayerPrefs.GetInt("BonusSpeed") == 2)
+            ms = 4;
+        else if (PlayerPrefs.GetInt("BonusSpeed") == 1)
+            ms = 2;
+        else
+            ms = 0;
+        // Голда
+        if (PlayerPrefs.GetInt("BonusGold") == 2)
+            gold = 10;
+        else if (PlayerPrefs.GetInt("BonusGold") == 1)
+            gold = 5;
+        else
+            gold = 0;
+
+
 
     }
 
     void Update()
     {
         moveInput = new Vector2(joystick.Horizontal, joystick.Vertical);
-        moveVelocity = moveInput.normalized * speed;
-        // Отображение хп
-        if ((health < 50) || (PlayerPrefs.GetInt("MaxHealth") == 2))
-            heart5.enabled = false;
-        else
-            heart5.enabled = true;
-
-        if ((health < 40) || (PlayerPrefs.GetInt("MaxHealth") == 1))
-            heart4.enabled = false;
-        else
-            heart4.enabled = true;
+        moveVelocity = moveInput.normalized * (speed + ms);
+        
     }
 
     void FixedUpdate()
@@ -85,7 +113,7 @@ public class Player : MonoBehaviour
         }
         else // Остальные 60%
         {
-            caz += 30;
+            caz += 30 + gold;
             PlayerPrefs.SetInt("money", caz); // золото
         }
         caz = 0;
@@ -95,8 +123,19 @@ public class Player : MonoBehaviour
     public void ChangeHealth(int healthValue)
     {
         health += healthValue; // получаемый урон/лечение
-
+        if (health > MaxHealth)
+            health = MaxHealth;
         // отображение хп
+        if (health < 50)
+            heart5.enabled = false;
+        else
+            heart5.enabled = true;
+
+        if (health < 40)
+            heart4.enabled = false;
+        else
+            heart4.enabled = true;
+
         if (health < 30)
             heart3.enabled = false;
         else

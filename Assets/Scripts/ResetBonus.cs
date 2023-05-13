26ea1e4090c580b6e0e5eac72abf14a3110f67f8
andrew09090870 <andrew09090870@yandex.ru>
2023-05-14 01:56:18 +0700

    Добавил кнопку сброса прокачки

---
+++ Assets/Scripts/ResetBonus.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetBonus : MonoBehaviour
+{
+    public void OnClick()
+    {
+        PlayerPrefs.SetInt("BonusHealth", 0);
+        PlayerPrefs.SetInt("BonusSoul", 0);
+        PlayerPrefs.SetInt("BonusGold", 0);
+        PlayerPrefs.SetInt("BonusSpeed", 0);
+    }
+}

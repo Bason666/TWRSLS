deac27f38de32536489d361382e2f4132f3b52d3
andrew09090870 <andrew09090870@yandex.ru>
2023-05-18 23:24:54 +0700

    Ну типа рабочий босс

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -13,13 +13,15 @@ public class Enemy : MonoBehaviour
     public int enemyDamage;
     private float timeBtwAttack;
     public float startTimeBtwAttack;
-    public Transform attackPos1;
     public LayerMask igrok;
     public float attackRangeEnemy;
-    // public Button MagicB;
     public bool Boss;
+
+    private Animator anim;
+
     private void Start()
     {
+        anim = GetComponent<Animator>();
         player = FindObjectOfType<Player>();
         if (PlayerPrefs.GetInt("BonusGold") == 2)
             bonus = 2;
@@ -32,15 +34,7 @@ public class Enemy : MonoBehaviour
     
     private void Update()
     {
-      //  if ((health <= 10))
-      //  {
-      //      MagicB.interactable = true;
-      //  }
-      //  else
-       // {
-        //    MagicB.interactable = false;
-       // }
-
+    
 
         if (health <= 0)
         {
@@ -63,29 +57,32 @@ public class Enemy : MonoBehaviour
        
         transform.position = Vector2.MoveTowards(transform.position, player.transform.position, speed * Time.deltaTime);
 
-        if (timeBtwAttack <= 0)
-        {
-                Collider2D[] enemies = Physics2D.OverlapCircleAll(attackPos1.position, attackRangeEnemy, igrok);
-            for (int i = 0; i < enemies.Length; i++)
-            {
-                enemies[i].GetComponent<Player>().ChangeHealth(-enemyDamage);
-            }
-                timeBtwAttack = startTimeBtwAttack;
-        }
-        else
-            timeBtwAttack -= Time.deltaTime;
-
     }
 
-    private void OnDrawGizmosSelected()
+    public void OnTriggerStay2D(Collider2D other)
     {
-        Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(attackPos1.position, attackRangeEnemy);
+        if (other.CompareTag("Player"))
+        {
+            if (timeBtwAttack <= 0)
+            {
+                anim.SetTrigger("EnemyAttack");
+            }
+            else
+            {
+                timeBtwAttack -= Time.deltaTime;
+            }
+        }
+    }
+
+    public void OnEnemyAttack()
+    {
+        player.GetComponent<Player>().ChangeHealth(-enemyDamage);
+       // player.health -= enemyDamage;
+        timeBtwAttack = startTimeBtwAttack;
     }
     public void TakeDamage(int damage)
     {
         health -= damage;
-
 
     }
 
